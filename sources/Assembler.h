0d66d6d4cbd2478b3aca79ced94a97979e5a7216
MohamedTolba <mohamedtolba96@hotmail.com>
2017-05-01 19:02:44 +0200

    Adding Assembler Singleton class

---
+++ sources/Assembler.h
@@ -0,0 +1,26 @@
+//
+// Created by Mohamed A.Tolba on 5/1/2017.
+//
+
+#ifndef SIC_XE_ASSEMBLER_ASSEMBLER_H
+#define SIC_XE_ASSEMBLER_ASSEMBLER_H
+
+#include <string>
+#include <map>
+
+class Assembler {
+
+public:
+    ~Assembler();
+    static Assembler *getInstance();
+    void execute(std::string fileName, std::map<std::string, std::string> options);
+
+private:
+    Assembler();
+    static Assembler *assembler;
+    std::string executePass1(std::string fileName, std::map<std::string, std::string> options);
+    void executePass2(std::string intermediateFileName);
+
+};
+
+#endif //SIC_XE_ASSEMBLER_ASSEMBLER_H
