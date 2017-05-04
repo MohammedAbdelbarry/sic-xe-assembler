9630f28d85cf91b235bf161b007854b849520f0f
Anas Harby <anas.harby@gmail.com>
2017-05-04 17:25:53 +0200

    Fixed some minor bugs, modified pass 1 to execute the directive whenever it finds one

--- sources/Assembler.cpp
+++ sources/Assembler.cpp
@@ -8,8 +8,14 @@
 #include <iomanip>
 #include <iostream>
 #include "../headers/Line.h"
+#include "../headers/enums.h"
+#include "../headers/Error.h"
 #include "../headers/strutil.h"
 #include "../headers/Assembler.h"
+#include "../headers/validator.h"
+#include "../headers/SymbolTable.h"
+#include "../headers/DirectiveInfo.h"
+#include "../headers/DirectiveTable.h"
 
 Assembler *Assembler::instance = nullptr;
 
@@ -27,7 +33,8 @@ Assembler::Assembler() {
 }
 
 std::string executePass1(std::string fileName, std::map<std::string, std::string> options,
-                         std::vector<Line> &lines, std::string &programName, int &programStart, int &locctr) {
+                         std::vector<Line> &lines, std::string &programName, int &programStart,
+                         int &locctr, SymbolTable &symbolTable) {
     std::string intermediateFile;
     std::string lineString;
     std::ifstream file(fileName);
@@ -35,19 +42,31 @@ std::string executePass1(std::string fileName, std::map<std::string, std::string
     while (std::getline(file, lineString)) {
         std::vector<std::string> lineVector = strutil::split(lineString, regex);
         Line line(lineVector[0], lineVector[1], lineVector[2]);
-        //TODO: CHECK FOR DIRECTIVES AND POPULATE SYMBOL TABLE.
-        //TODO: Add line to intermediate file if it's valid.
+        if (validator::isValidLine(line)) {
+            if (DirectiveTable::getInstance()->contains(line.operation)) {
+                //Directive line.
+                DirectiveTable::getInstance()->getInfo(line.operation).execute(locctr, line);
+                symbolTable.push(line.operation, locctr);
+            } else { //Instruction line.
+                //TODO: Check if format is valid and increment locCtr accordingly.
+            }
+            //TODO: Add line to intermediate file if it's valid.
+        } else {
+            line.error = new Error(ErrorMessage::INVALID_LABEL);
+            //TODO: temporary until we figure out the exception thingy.
+        }
         lines.push_back(line);
     }
     return intermediateFile;
 }
 
 void executePass2(std::string intermediateFileName, std::vector<Line> &lines, std::string programName, int programStart, int locctr) {
-    //TODO implement this method
     int length = locctr - programStart;
     std::ostringstream objCodeStream;
     objCodeStream << "H";
-    objCodeStream << std::setw(6) << std::left << programName;
+//    objCodeStream << std::setw(6) << std::left << programName;
+    objCodeStream << std::setw(6) << std::left;
+    objCodeStream << programName;
 //    objCodeStream << std::hex << std::setfill('0') << std::setw(6) << std::right << std::uppercase << programStart;
     strutil::addHex(objCodeStream, programStart, 6);
     strutil::addHex(objCodeStream, length, 6);
@@ -67,6 +86,7 @@ void Assembler::execute(std::string fileName, std::map<std::string, std::string>
     int programStart = 0;
     std::string programName = "";
     std::vector<Line> lines;
-    std::string intermediateFile = executePass1(fileName, options, lines, programName, programStart, locctr);
+    SymbolTable symbolTable;
+    std::string intermediateFile = executePass1(fileName, options, lines, programName, programStart, locctr, symbolTable);
     executePass2(intermediateFile, lines, programName, programStart, locctr);
 }
