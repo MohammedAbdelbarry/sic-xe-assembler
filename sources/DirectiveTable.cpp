8cf2833e6eabbff9d1a33c2753a64c29326aee78
Mohammed Abdelbarry <m7md337@gmail.com>
2017-05-04 10:33:02 +0200

    Implemented some of the functions in the directive table

--- sources/DirectiveTable.cpp
+++ sources/DirectiveTable.cpp
@@ -21,40 +21,69 @@ DirectiveTable* DirectiveTable::getInstance() {
 void DirectiveTable::initDirTable() {
     std::string dirName;
     DirectiveInfo info;
-
+    const char HEX_BASE = 16;
     dirName = "START";
     info.label = Label::OPTIONAL;
-    info.execute = [&](int &locCtr, Line) {
+    info.execute = [](int &locCtr, Line line) {
+        try {
+            //TODO: SET PROGRAM NAME = LABEL
+            int pos = std::stoi(line.operand, 0, HEX_BASE);
+            locCtr = pos;
+        } catch(std::invalid_argument ex) {
+
+        } catch(std::out_of_range ex) {
+
+        }
         //TODO: set locCtr to the specified starting address.
     };
 
     dirName = "BYTE";
-    info.execute = [&](int &locCtr, Line) {
+    info.execute = [](int &locCtr, Line) {
+        //TODO: check if the label is valid
+        //TODO: check if the operand is a valid literal
         locCtr++;
     };
     dirTable[dirName] = info;
 
     dirName = "WORD";
-    info.execute = [&](int &locCtr, Line) {
+    info.execute = [](int &locCtr, Line) {
+        //TODO: check if the label is valid
+        //TODO: check if the operand is a valid literal
         locCtr += 3;
     };
     dirTable[dirName] = info;
 
     dirName = "RESB";
-    info.execute = [&](int &locCtr, Line) {
+    info.execute = [](int &locCtr, Line line) {
+        try {
+            int count = std::stoi(line.operand);
+            locCtr += count;
+        } catch(std::invalid_argument ex) {
+
+        } catch(std::out_of_range ex) {
+
+        }
         //TODO: append locCtr by number of bytes specified.
     };
     dirTable[dirName] = info;
 
     dirName = "RESW";
-    info.execute = [&](int &locCtr, Line) {
+    info.execute = [](int &locCtr, Line line) {
+        try {
+            int count = std::stoi(line.operand);
+            locCtr += count * 3;
+        } catch(std::invalid_argument ex) {
+
+        } catch(std::out_of_range ex) {
+
+        }
         //TODO: append locCtr by 3 * number of words specified.
     };
     dirTable[dirName] = info;
 
     dirName = "END";
-    info.execute = [&](int &locCtr, Line) {
-        //TODO: dk lol.
+    info.execute = [](int &locCtr, Line) {
+        //TODO: optional operand marks the start of the program :D
     };
     dirTable[dirName] = info;
 }
