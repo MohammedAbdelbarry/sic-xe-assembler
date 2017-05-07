9fd14ebbd3d3ebe8ce90fc0bab8ffc416674a069
Eng Salem <Eng Salem>
2017-05-07 21:57:53 +0200

    Documented Directive table/info

--- headers/DirectiveInfo.h
+++ headers/DirectiveInfo.h
@@ -1,6 +1,7 @@
-//
-// Created by Mohammed Abdelbarry on 01-May-17.
-//
+/**
+ * @file DirectiveInfo.h
+ * Represents info for each directive (label, function that updates location counter).
+ */
 
 #ifndef SIC_XE_ASSEMBLER_DIRECTIVE_INFO_H
 #define SIC_XE_ASSEMBLER_DIRECTIVE_INFO_H
@@ -9,10 +10,21 @@
 #include "enums.h"
 #include "Line.h"
 
+/**
+ * Represents info for each directive, contains label for the directive and a<br>
+ * function that updtes location counter accordingly.
+ */
 class DirectiveInfo {
 
 public:
+	/**
+	 * Label of the corresponding directive.
+	 */
     Label label;
+    /**
+     * A function that is defined for each directive. Takes in the location<br>
+     * counter and updates it accordingly based on the line passed to the function.
+     */
     std::function<void(int&, Line)> execute;
 };
 #endif //SIC_XE_ASSEMBLER_DIRECTIVE_INFO_H
